c11b101f42f8f9be8723be02662ef23e7dc7f1e9
fabiogueunige <s5004782@studenti.unige.it>
2024-01-12 14:51:05 +0100

    End of first node and start of the second one

--- scripts/action_client.py
+++ scripts/action_client.py
@@ -1,44 +1,157 @@
 #! /usr/bin/env python3
 
 import rospy
-from geometry_msgs.msg import Point, Pose, Twist
-from sensor_msgs.msg import LaserScan
+#from geometry_msgs.msg import Point, Pose, Twist
 from nav_msgs.msg import Odometry
-import math
 import actionlib
 import actionlib.msg
 import assignment_2_2023.msg
-from tf import transformations
-from std_srvs.srv import *
-import time
+from assignment_2_2023.msg import Robotinfo
+from assignment_2_2023.msg import PlanningAction, PlanningGoal, PlanningResult, PlanningActionResult
+from actionlib_msgs.msg import GoalStatus
 
+# Global variables
+pubInfo = None
+subOdom = None  
 
-# Add the cycle to 
+#def OdomCallback(msg):
+    # Get the actual pose 
+
+    # Put it in the message
+
+def insertNumber():
+    """
+    Function to take the input from the user to know the coordinates of the goal
+
+    Returns
+    -------
+    inpx : float
+        x coordinate of the goal    
+    inpy : float    
+        y coordinate of the goal
+    """
+    while(1):
+        inpx = input("x: ")
+        try:
+            inpx = float(inpx)
+            # to exit form the while loop
+            break
+        except:
+            print("Please insert a valid number")
+    while(1):
+        inpy = input("y: ")
+        try:
+            inpy = float(inpy)
+            break
+        except:
+            print("Please insert a valid number")
+    return inpx, inpy
+
+def odom_callback(msg):
+    """
+    Callback function for the subscriber of the odometry
+
+    Parameters
+    ----------
+    msg : Odometry
+        message from the subscriber
+    """
+    global pubInfo
+
+    # Create the message to publish
+    msgInfo = Robotinfo()
+    msgInfo.pos_x = msg.pose.pose.position.x
+    msgInfo.pos_y = msg.pose.pose.position.y
+    msgInfo.vel_x = msg.twist.twist.linear.x
+    msgInfo.vel_z = msg.twist.twist.linear.z
+
+    # Publish the message
+    pubInfo.publish(msgInfo)
+
 def action_client():
-    # Create the connection to the action server.
-    client = actionlib.SimpleActionClient('target_server',assignment_2_2023.msg.PlanningAction)
-    # Wait until the action server is up and running.
-    client.wait_for_server()
+    """
+    Function to implement the action client
+    """
 
-    goal = assignment_2_2023.msg.PlanningGoal() # Check
-    # Define the goal
-    goal.target_pose.pose.position.x = 10
-    goal.target_pose.pose.position.y = 10
-    # Send the goal
-    client.send_goal(goal)
-    # Wait for the result
-    client.wait_for_result()
+    # Create the connection to the action server.
+    clienttar = actionlib.SimpleActionClient('/reaching_goal',assignment_2_2023.msg.PlanningAction)
+    
+    # Wait until the action server is up and running.
+    clienttar.wait_for_server()
+
+    # User interface
+    rospy.loginfo("Hi, welcome to the robot planner simulation")
+    rospy.loginfo("Please choose the goal you want to reach")
+    rospy.loginfo("Insert the coordinates to reach as ")
+    
+    # Create a goal to send (to the action server)
+    goal = assignment_2_2023.msg.PlanningGoal()
+
+    # implements the coordinates acquisition
+    inpx, inpy = insertNumber()
+
+    # Update the goal
+    goal.target_pose.pose.position.x = inpx
+    goal.target_pose.pose.position.y = inpy
+
+    # Sending the goal
+    clienttar.send_goal(goal)
+    rospy.loginfo("You sent the goal with: X = %f, Y = %f", goal.target_pose.pose.position.x, goal.target_pose.pose.position.y)
+
+    while not rospy.is_shutdown(): # can sobstitute with rospy.spin()
+        # Define the goal (will be passed from the user)
+        rospy.loginfo("Please choose the new goal you want to reach or cancel the previouse one")
+        choice = input("Do you want to insert a new goal? (y/n)")
+        if (choice == 'y'):
+            # implements the coordinates acquisition
+            inpx, inpy = insertNumber()
+
+            # Update the goal
+            goal.target_pose.pose.position.x = inpx
+            goal.target_pose.pose.position.y = inpy
+
+            # Send the goal
+            clienttar.send_goal(goal)
+            rospy.loginfo("You sent the goal with: X = %f, Y = %f", goal.target_pose.pose.position.x, goal.target_pose.pose.position.y)
+        else:
+            choice = input("Do you want to cancel the previouse goal and restore ? (y/n)")
+            if (choice == 'y'):
+                # add the control to the status of the goal!!
+                if (clienttar.get_state() == GoalStatus.ACTIVE):
+                    rospy.loginfo("You canceled the goal with: X = %f, Y = %f", goal.target_pose.pose.position.x, goal.target_pose.pose.position.y)
+                    clienttar.cancel_goal() 
+                else:
+                    rospy.loginfo("The goal has already finished or it was already canceled")
+            else:
+                rospy.loginfo("The goal is still active with X = %f, Y = %f", goal.target_pose.pose.position.x, goal.target_pose.pose.position.y)
+        
+   
     # Return the result
-    return client.get_result()
+    # return client.get_result()
+
+def main():
+    """
+    Main function
+    """
+    global pubInfo, subOdom
+    # Initialize the node
+    rospy.init_node('action_client')
+
+    # Call the publisher for velocity and position
+    pubInfo =  rospy.Publisher('/robot_info', Robotinfo, queue_size=1)
+
+    # Call the subscriber from Odom for position and velocity
+    subOdom = rospy.Subscriber('/odom', Odometry, odom_callback)
+
+    # Call the function
+    action_client()
 
 if __name__ == '__main__':
     try:
         # Initialize the node
-        rospy.init_node('action_client')
-        # Call the function
-        result = action_client()
+        main()
         # Print the result given by the action server
-        print("Result: ",result)
     except rospy.ROSInterruptException:
-        print("program interrupted before completion", file=sys.stderr)
+        print("Errors in action_client.py")
+        print("program interrupted before completion for errors", file=sys.stderr)
         pass
--- scripts/last_tar_service.py
+++ scripts/last_tar_service.py
@@ -1,2 +1,81 @@
 #! /usr/bin/env python3
 
+import rospy
+from assignment_2_2023 import srv
+from assignment_2_2023 import msg
+from assignment_2_2023.srv import LastTarget, LastTargetResponse
+from assignment_2_2023.msg import PlanningActionGoal, PlanningGoal
+
+
+subgoal = None
+servicetarget = None
+
+# /reaching_goal/goal
+def target_service_callback(request):
+    """
+    Callback function for the service server of the last target
+
+    Parameters
+    ----------
+    request : LastTarget
+        request from the service server
+
+    Returns
+    -------
+    LastTargetResponse
+        response from the service server
+    """
+    global subgoal
+    global postarx, postary
+
+    # Create the response
+    resp = LastTargetResponse()
+    resp.tar_pose_x = postarx
+    resp.tar_pose_y = postary
+
+    # Return the response
+    return resp
+    
+def target_sub_callback(msg):
+    """
+    Callback function for the subscriber of the goal
+
+    Parameters
+    ----------
+    msg : PlanningGoal
+        message from the subscriber
+    """
+    global subgoal
+    global postarx, postary
+
+    postarx = msg.goal.position.x
+    postary = msg.goal.position.y
+
+
+def last_tar_service():
+    """
+    Main function for the service server of the last target
+    """
+    global subgoal, servicetarget
+
+    # Initialize the node
+    rospy.init_node('last_target')
+
+    # Create the subscriber
+    subgoal = rospy.Subscriber('/reaching_goal/goal', PlanningActionGoal, target_sub_callback)
+    # Create the service
+    servicetarget = rospy.Service('/last_target', LastTarget, target_service_callback)
+    
+    # Spin
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    try:
+        # Initialize the node
+        last_tar_service()
+        # Print the result given by the action server
+    except rospy.ROSInterruptException:
+        print("Errors in last_tar_service.py")
+        print("program interrupted before completion for errors", file=sys.stderr)
+        pass
