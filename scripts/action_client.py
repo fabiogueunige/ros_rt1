bd83762066d874cf3f243c7e3662b6c638ed9497
fabiogueunige <s5004782@studenti.unige.it>
2024-04-10 19:30:53 +0200

    versione dove si cancellano e si aggiungono i goal da user interface

--- scripts/action_client.py
+++ scripts/action_client.py
@@ -38,37 +38,9 @@ import time
 # Global variables
 pubInfo = None
 subOdom = None  
-clienttar = None
+# clienttar = None
 pubInfoFeet = None
-servCancGoal = None
-
-
-def insertNumber():
-    """
-    Function to take the input from the user to know the coordinates of the goal
-
-    Returns
-    -------
-    inpx : float
-        x coordinate of the goal    
-    inpy : float    
-        y coordinate of the goal
-    """
-    while(1):
-        inpx = input("x: ")
-        try:
-            inpx = float(inpx)
-            break
-        except:
-            print("Please insert a valid number")
-    while(1):
-        inpy = input("y: ")
-        try:
-            inpy = float(inpy)
-            break
-        except:
-            print("Please insert a valid number")
-    return inpx, inpy
+# servCancGoal = None
 
 def odom_callback(msg):
     """
@@ -96,109 +68,18 @@ def odom_callback(msg):
     pubInfoFeet.publish(msgFeet)
 
 
-def goal_canceling_service(req):
-    """
-    Service to cancel the goal
-    
-    Args:
-    req (CancelGoal): request to cancel the goal
-    """
-    
-    global servCancGoal
-
-    rospy.wait_for_service('/cancel_goal')
-
-    response = CancelGoalResponse()
-
-    
-    try: 
-        clienttar.cancel_goal() 
-        response.stat = "Done"
-    except:
-        response.stat = "Error"
-    
-    return response
-
-
-
-
-def action_client():
-    """
-    Function to implement the action client
-    """
-
-    global clienttar, servCancGoal
-
-    # Wait until the action server is up and running.
-    clienttar.wait_for_server()
-    
-
-    # User interface
-    print("Hi, welcome to the robot planner simulation")
-    print("Please choose the position you want to reach with the robot")
-    print("Insert the coordinates to reach as ")
-    
-    # Create a goal to send (to the action server)
-    goal = assignment_2_2023.msg.PlanningGoal()
-
-    # Implements the coordinates acquisition
-    inpx, inpy = insertNumber()
-
-    # Update the goal
-    goal.target_pose.pose.position.x = inpx
-    goal.target_pose.pose.position.y = inpy
-
-    # Sending the goal
-    clienttar.send_goal(goal)
-    print("You sent the goal with: X = ", goal.target_pose.pose.position.x," Y = ", goal.target_pose.pose.position.y)
-
-    while not rospy.is_shutdown(): 
-        # Input from the user
-        choice = input("Do you want to insert a new goal? (y/n) ")
-        if (choice == 'y'):
-            # Implements the coordinates acquisition
-            inpx, inpy = insertNumber()
-
-            # Update the goal
-            goal.target_pose.pose.position.x = inpx
-            goal.target_pose.pose.position.y = inpy
-
-            # Send the goal
-            clienttar.send_goal(goal)
-            print("You sent the goal with: X = ", goal.target_pose.pose.position.x," Y = ", goal.target_pose.pose.position.y)
-        else:
-            choice = input("Do you want to cancel the previouse goal and restore ? (y/n) ")
-            if (choice == 'y'):
-                if (clienttar.get_state() == GoalStatus.ACTIVE):
-                    print("You canceled the goal with: X = ", goal.target_pose.pose.position.x," Y = ", goal.target_pose.pose.position.y)
-                    clienttar.cancel_goal() 
-                else:
-                    print("The goal has already finished or it was already canceled")
-            else:
-                print("No Operations has been done")
-
 def main():
     """
         This function initializes the nodes and calls the action client function.
         This also initializes the publisher and subscriber for the robot information, the service for the goal canceling and the action to reach the goal.
     """
-    global pubInfo, subOdom, clienttar, pubInfoFeet, servCancGoal
+    global pubInfo, subOdom, pubInfoFeet
     
     # Wait for gazebo to be up and running
     time.sleep(1)
 
     # Initialize the node
-    rospy.init_node('action_client')
-
-    # Create the service Client
-    clienttar = actionlib.SimpleActionClient('/reaching_goal',assignment_2_2023.msg.PlanningAction)
-    """ Client for the action server reaching_goal
-    """
-
-    # Service to cancel the goal
-    servCancGoal = rospy.ServiceProxy('/cancel_goal', CancelGoal) #  goal_canceling_service va aggiunto?
-    """ Service to cancel the goal
-    """
+    rospy.init_node('action_client') 
 
     # Call the publisher for velocity and position
     pubInfo =  rospy.Publisher('/robot_info', Robotinfo, queue_size=1)
@@ -215,8 +96,7 @@ def main():
     """ Publisher for the robot information in feet
     """
 
-    # Call the function
-    action_client()
+    rospy.spin() # Keeps the node running
 
 if __name__ == '__main__':
     try:
