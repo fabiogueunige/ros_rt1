0d7f12cc00c521fc215200a9b5c711af27e7a58f
fabiogueunige <s5004782@studenti.unige.it>
2024-04-10 22:57:28 +0200

    using as user interface publishing goal as message

--- scripts/action_client.py
+++ scripts/action_client.py
@@ -13,6 +13,8 @@ It also publishes the position and velocity of the robot.
 
 Subscribes to:
     /odom
+    /goal_coord
+    /goal_cancel
 
 Publishes to:
     /robot_info
@@ -30,7 +32,7 @@ from nav_msgs.msg import Odometry
 import actionlib
 import actionlib.msg
 import assignment_2_2023.msg
-from assignment_2_2023.msg import Robotinfo, Robotinfofeet
+from assignment_2_2023.msg import Robotinfo, Robotinfofeet, GoalCoord
 from actionlib_msgs.msg import GoalStatus
 from assignment_2_2023.srv import CancelGoal, CancelGoalResponse
 import time
@@ -38,16 +40,19 @@ import time
 # Global variables
 pubInfo = None
 subOdom = None  
-# clienttar = None
+clienttar = None
 pubInfoFeet = None
-# servCancGoal = None
+servCancGoal = None
+goalpos = None
+
+# Subscriber callback declarations
 
 def odom_callback(msg):
     """
     Callback function for the subscriber of the odometry
 
     Args:
-    msg (Odometry): message from the subscriber for the odometry
+    msg (Odometry): message from the publisher for the odometry
     """
     global pubInfo, pubInfoFeet
 
@@ -68,18 +73,76 @@ def odom_callback(msg):
     pubInfoFeet.publish(msgFeet)
 
 
+def gcoord_callback(msg):
+    """
+    Callback function for the subscriber of the goal position
+
+    Args:
+    msg (GoalCoord): message from the publisher for the goal position
+    """
+    global clienttar
+
+    goal = assignment_2_2023.msg.PlanningGoal()
+
+    goal.target_pose.pose.position.x = msg.coord_x
+    goal.target_pose.pose.position.y = msg.coord_y
+
+    # Sending the goal
+    clienttar.send_goal(goal)
+    
+
+# Service callback declarations
+
+def goal_canceling_service(req):
+    """
+    Service to cancel the goal
+    
+    Args:
+    req (CancelGoal): request to cancel the goal
+    """
+    
+    global clienttar
+
+    rospy.wait_for_service('/cancel_goal')
+
+    response = CancelGoalResponse()
+
+    
+    try: 
+        clienttar.cancel_goal() 
+        response.stat = "Done"
+    except:
+        if (clienttar.get_state() != GoalStatus.ACTIVE):
+            response.stat = "Goal already reached"
+        else:
+            response.stat = "Error"
+    
+    return response
+
+
 def main():
     """
         This function initializes the nodes and calls the action client function.
         This also initializes the publisher and subscriber for the robot information, the service for the goal canceling and the action to reach the goal.
     """
-    global pubInfo, subOdom, pubInfoFeet
+    global pubInfo, subOdom, clienttar, pubInfoFeet, servCancGoal
+    global goalpos
     
     # Wait for gazebo to be up and running
     time.sleep(1)
 
     # Initialize the node
-    rospy.init_node('action_client') 
+    rospy.init_node('action_client')
+
+    # Create the service Client
+    clienttar = actionlib.SimpleActionClient('/reaching_goal',assignment_2_2023.msg.PlanningAction)
+    """ Client for the action server reaching_goal
+    """
+
+    # Service to cancel the goal
+    servCancGoal = rospy.ServiceProxy('/cancel_goal', CancelGoal) #  goal_canceling_service va aggiunto?
+    """ Service to cancel the goal
+    """
 
     # Call the publisher for velocity and position
     pubInfo =  rospy.Publisher('/robot_info', Robotinfo, queue_size=1)
@@ -96,7 +159,15 @@ def main():
     """ Publisher for the robot information in feet
     """
 
-    rospy.spin() # Keeps the node running
+    # Subscriber for the goal position
+    goalpos = rospy.Subscriber('/goal_coord', GoalCoord, gcoord_callback)
+    """ Subscriber for the goal position
+    """
+    
+    clienttar.wait_for_server()
+
+    # Keep the node running
+    rospy.spin()
 
 if __name__ == '__main__':
     try:
